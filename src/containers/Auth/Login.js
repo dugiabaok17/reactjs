0c2893401b8e2de1a4abed8c2e96ce3e36237858
dugiabaok17 <andvph27911@gmail.com>
2023-02-26 14:21:27 +0700

    second

--- src/containers/Auth/Login.js
+++ src/containers/Auth/Login.js
@@ -5,11 +5,31 @@ import { push } from "connected-react-router";
 import * as actions from "../../store/actions";
 
 import "./Login.scss";
-
 class Login extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      username: "",
+      password: "",
+      isShowPassword: false,
+    };
   }
+
+  handleOnChangeInput = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+    console.log(e.target.value);
+  };
+  handleLogin = () => {
+    console.log(this.state);
+  };
+
+  handleShowHidePassword = () => {
+    this.setState({
+      isShowPassword: !this.state.isShowPassword
+    })
+  };
 
   render() {
     return (
@@ -23,30 +43,39 @@ class Login extends Component {
                 type="text"
                 className="form-control"
                 placeholder="Enter your username"
+                name="username"
+                value={this.state.username}
+                onChange={(e) => this.handleOnChangeInput(e)}
               />
             </div>
             <div className="col-12 form-group login-input">
               <label>Password:</label>
               <input
-                type="password"
+                type={this.state.isShowPassword === false ? 'password' : 'text'}
                 className="form-control"
                 placeholder="Enter your password"
+                name="password"
+                value={this.state.password}
+                onChange={(e) => this.handleOnChangeInput(e)}
               />
+              <span onClick={() => this.handleShowHidePassword()}>
+                  <i className= {this.state.isShowPassword === true ? "fas fa-eye custom-location": "fas fa-eye-slash custom-location"} ></i>
+              </span>
             </div>
             <div className="col-12">
-              <button className="btn-login">Login</button>
+              <button className="btn-login" onClick={() => this.handleLogin()}>
+                Login
+              </button>
             </div>
-            <div className="col-12">
+            <div className="col-12 mt-1">
               <span className="forgot-password">Forget your password ?</span>
             </div>
-            <div className="col-12 text-center">
-                <span className="text-other-login">
-                    Or login with:
-                </span>
+            <div className="col-12 text-center mt-2">
+              <span className="text-other-login">Or login with:</span>
             </div>
             <div className="col-12 social-login ">
-            <i className="fab fa-google-plus-g google"></i>
-            <i className="fab fa-facebook-f facebook"></i>
+              <i className="fab fa-google-plus-g google"></i>
+              <i className="fab fa-facebook-f facebook"></i>
             </div>
           </div>
         </div>
